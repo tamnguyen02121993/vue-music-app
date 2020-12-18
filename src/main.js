32d32a83c0b4b83e908a3af9f5b0c2141cab60d0
tamnguyen02121993 <tamnguyen02121993@gmail.com>
2020-12-19 01:44:58 +0700

    Add UI

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import PrimeVue from "primevue/config";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
+import FileUpload from "primevue/fileupload";
+import Sidebar from "primevue/sidebar";
+import Card from "primevue/card";
+import Listbox from "primevue/listbox";
+import Divider from "primevue/divider";
+import Button from "primevue/button";
+import Slider from "primevue/slider";
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+// Css
+import "primevue/resources/themes/bootstrap4-light-purple/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "@/assets/scss/custom.scss";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(PrimeVue, { ripple: true })
+  .use(ToastService)
+  .component("toast", Toast)
+  .component("file-upload", FileUpload)
+  .component("side-bar", Sidebar)
+  .component("card", Card)
+  .component("list-box", Listbox)
+  .component("divider", Divider)
+  .component("p-button", Button)
+  .component("slider", Slider)
+  .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,104 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-grid">
+    <div class="p-col-6 p-offset-3">
+      <card class="h-35vh">
+        <template #title>
+          <div class="text-primary">
+            <i class="material-icons">graphic_eq</i> Music
+          </div>
+        </template>
+        <template #content>
+          <div class="p-text-center p-text-uppercase text-primary p-mb-4">
+            Lily
+          </div>
+          <div class="p-d-flex p-jc-center p-ai-center p-mb-2">
+            <div>
+              <p-button
+                class="p-button-sm p-button-outlined p-button-text p-mr-1"
+                ><i class="material-icons md-18">playlist_add</i></p-button
+              >
+              <p-button
+                class="p-button-sm p-button-outlined p-button-text p-mr-1"
+                ><i class="material-icons md-18">skip_previous</i></p-button
+              >
+              <p-button
+                class="p-button-sm p-button-outlined p-button-text p-mr-1"
+                ><i class="material-icons md-18">pause</i></p-button
+              >
+              <p-button
+                class="p-button-sm p-button-outlined p-button-text p-mr-1"
+                ><i class="material-icons md-18">play_arrow</i></p-button
+              >
+              <p-button
+                class="p-button-sm p-button-outlined p-button-text p-mr-1"
+                ><i class="material-icons md-18">skip_next</i></p-button
+              >
+              <p-button
+                class="p-button-sm p-button-outlined p-button-text p-mr-1"
+                ><i class="material-icons md-18">volume_off</i></p-button
+              >
+            </div>
+          </div>
+
+          <div class="p-d-flex p-jc-center p-ai-center p-mb-2">
+            <slider v-model="currentTrack" :min="0" :max="100" />
+          </div>
+          <div class="p-d-flex p-jc-between p-ai-center p-mb-4">
+            <div>0</div>
+            <div>100</div>
+          </div>
+          <div class="p-d-flex p-jc-center p-ai-center">
+            <p-button class="p-button-sm p-button-outlined p-button-text p-mr-1"
+              ><i class="material-icons md-18">shuffle</i></p-button
+            >
+            <p-button class="p-button-sm p-button-outlined p-button-text p-mr-1"
+              ><i class="material-icons md-18">replay</i></p-button
+            >
+            <p-button class="p-button-sm p-button-outlined p-mr-1 p-button-text"
+              ><i class="material-icons md-18">volume_down</i></p-button
+            >
+            <slider v-model="volume" :min="0" :max="100" style="width: 7rem" />
+            <p-button
+              class="p-button-sm p-button-outlined p-mr-1 p-button-text p-ac-start"
+              ><i class="material-icons md-18">volume_up</i></p-button
+            >
+          </div>
+        </template>
+      </card>
+    </div>
+  </div>
+  <div class="p-col-6 p-offset-3 p-mt-5">
+    <list-box
+      v-model="selectedCity"
+      :options="cities"
+      optionLabel="name"
+      filter
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      volume: 50,
+      currentTrack: 0,
+      selectedCity: null,
+      cities: [
+        { name: "New York", code: "NY" },
+        { name: "Rome", code: "RM" },
+        { name: "London", code: "LDN" },
+        { name: "Istanbul", code: "IST" },
+        { name: "Paris", code: "PRS" },
+      ],
+    };
+  },
+  methods: {
+    onFilesSelected(event) {
+      console.log(event);
+      debugger;
+    },
+  },
+};
 </script>
